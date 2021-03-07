2cf539babfe3227b2447854a8b7ff65cd605d42a
Isaac L <isaac.longkutoy@gmail.com>
2021-03-07 18:06:08 -0500

    Done chapter 2 challenge: add a prev button

--- app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -33,25 +34,27 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
-
-            Toast.makeText(this,
-                    R.string.correct_toast,
-                    Toast.LENGTH_SHORT)
-                    .show()
         }
 
         falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
-
-
         }
 
         nextButton.setOnClickListener { view: View ->
             currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener { view: View ->
+            currentIndex -= 1
+            if (currentIndex < 0) {
+                currentIndex = questionBank.size - 1
+            }
             updateQuestion()
         }
 
@@ -63,7 +66,7 @@ class MainActivity : AppCompatActivity() {
         questionTextView.setText(questionTextResId)
     }
 
-    private fun checkAnswer(userAnswer: Boolean){
+    private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
 
         val messageResId = if (userAnswer == correctAnswer) {
