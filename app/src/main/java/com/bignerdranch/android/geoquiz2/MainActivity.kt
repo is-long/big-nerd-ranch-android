eed493cdd5f15bf4d05f8533e7490ccbf8f1457d
Isaac L <isaac.longkutoy@gmail.com>
2021-03-07 18:28:24 -0500

    Done chapter 2 challenge: from button to image button

--- app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,8 +13,8 @@ class MainActivity : AppCompatActivity() {
     // use lateinit = promise to compiler will provide non null values later
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private lateinit var prevButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -33,8 +34,8 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        nextButton = findViewById(R.id.next_button)
-        prevButton = findViewById(R.id.prev_button)
+        nextButton = findViewById(R.id.next_image_button)
+        prevButton = findViewById(R.id.prev_image_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
