896171131d1b520bfe94554c2aafe4f8a2c10490
Isaac L <isaac.longkutoy@gmail.com>
2021-03-07 12:20:06 -0500

    Done chapter 1 guide

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.geoquiz2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    // use lateinit = promise to compiler will provide non null values later
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            // pass
+            Toast.makeText(this,
+                    R.string.correct_toast,
+                    Toast.LENGTH_SHORT)
+                    .show()
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(this,
+            R.string.incorrect_toast,
+            Toast.LENGTH_SHORT)
+                    .show()
+        }
+    }
+}
