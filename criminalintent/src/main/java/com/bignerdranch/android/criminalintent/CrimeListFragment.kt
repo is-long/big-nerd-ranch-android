c887483dec41133343cb170eacc7bfd82e8d44a7
Isaac L <isaac.longkutoy@gmail.com>
2021-03-09 12:23:22 -0500

    Done chapter 10 challenge: formatting the date

--- criminalintent/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ criminalintent/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -81,7 +82,7 @@ class CrimeListFragment : Fragment() {
         fun bind(crime: Crime) {
             this.crime = crime
             titleTextView.text = this.crime.title
-            dateTextView.text = this.crime.date.toString()
+            dateTextView.text = DateFormat.format("EEEE, MMM dd, yyyy",this.crime.date)
 
             solvedImageView.visibility = if (crime.isSolved) {
                 View.VISIBLE
