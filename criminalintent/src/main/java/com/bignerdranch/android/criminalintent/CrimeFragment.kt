3a3073a19737996c97a221f462720223c39326bf
Isaac L <isaac.longkutoy@gmail.com>
2021-03-10 10:03:37 -0500

    Add date picker fragment

--- criminalintent/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ criminalintent/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -18,6 +18,7 @@ import java.util.*
 
 private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
+private const val DIALOG_DATE = "DialogDate"
 
 class CrimeFragment: Fragment() {
 
@@ -58,12 +59,6 @@ class CrimeFragment: Fragment() {
         titleField = view.findViewById(R.id.crime_title) as EditText
         dateButton = view.findViewById(R.id.crime_date) as Button
         solvedCheckBox =  view.findViewById(R.id.crime_solved) as CheckBox
-
-        dateButton.apply {
-            text = crime.date.toString()
-            isEnabled = false
-        }
-
 
         return view
     }
@@ -125,6 +120,14 @@ class CrimeFragment: Fragment() {
         solvedCheckBox.apply {
             setOnCheckedChangeListener { _, isChecked ->
                 crime.isSolved = isChecked
+            }
+        }
+
+        dateButton.setOnClickListener {
+            DatePickerFragment().apply {
+                // need non-null FM,  requireFragmentManager() returns non-null
+                // fragmentManager? might be null
+                show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
             }
         }
     }
