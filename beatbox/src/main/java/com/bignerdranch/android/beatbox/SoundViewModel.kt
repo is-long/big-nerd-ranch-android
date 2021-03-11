3db5a32d286a4b12a238cef8bb206d8eaf47e2b3
Isaac L <isaac.longkutoy@gmail.com>
2021-03-11 09:45:35 -0500

    Doen chapter 19

---
+++ beatbox/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.beatbox
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+// let VM comms w/ layout file when change occurs by implementing Observable()
+class SoundViewModel: BaseObservable() {
+    var sound: Sound? = null
+        set(sound) {
+            field = sound
+            // call each time property value change
+            // the binding class then reruns to repopulate view
+            notifyChange()
+
+            // use this one if only specific (not all) value changes
+            // notifyPropertyChanged()
+        }
+
+    @get:Bindable
+    val title: String?
+        get() = sound?.name
+}
